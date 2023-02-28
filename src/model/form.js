c949cb8860f627610964fcbe770cf7641dade4a3
dcrana <divyarajsinhrana2487@gmail.com>
2023-02-28 17:29:14 +0530

    cors

--- src/model/form.js
+++ src/model/form.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const formSchema = new mongoose.Schema({
-  dept: { type: String , required:true},
-  subDept: { type: String , required:true},
-  jsonForm: { type: String , required:true},
+  dept: { type: String, required: true },
+  subDept: { type: String, required: true },
+  jsonForm: { type: String, required: true },
 });
 
 
-module.exports = mongoose.model('JSONForm', formSchema )
+module.exports = mongoose.model('JSONForm', formSchema)
--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require('cors');
 const { default: mongoose } = require("mongoose");
-const { JSONForm, UserFormData } = require("./model/form");
+const JSONForm = require("./model/form");
+const { UserFormData } = require('./model/userFormData')
+
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
+
+// create nwe form 
 app.post("/create-form", async (req, res) => {
   const { dept, subDept, jsonForm } = req.body;
 
@@ -15,20 +21,74 @@ app.post("/create-form", async (req, res) => {
     jsonForm,
   });
 
-  return res.status(201).json({ msg: "form created" });
+  res.status(201).json({ msg: "form created", formId: form._id });
 });
 
+
+
+// to get fomr with id
+app.get('', async (req, res) => {
+  res.send('Welcome to server')
+})
+
+app.get('/get-form/:id', async (req, res) => {
+
+  try {
+
+    const form = await JSONForm.findOne({ _id: req.params.id })
+
+    if (form) {
+      res.status(200).json({ msg: form })
+    } else {
+      res.status(200).json({ msg: 'no data found' })
+    }
+
+  } catch (error) {
+
+    res.send('something went workng')
+
+  }
+
+
+})
+
+
+
+// to get fomr with id
+app.get('/get-user-form/:id', async (req, res) => {
+
+  try {
+
+    const form = await UserFormData.findOne({ _id: req.params.id })
+
+    if (form) {
+      res.status(200).json({ msg: form })
+    } else {
+      res.status(200).json({ msg: 'no data found' })
+    }
+
+  } catch (error) {
+
+    res.send('something went workng')
+
+  }
+
+
+})
+
+
+// to add user data againts any form 
 app.post("/add-user-form", async (req, res) => {
   const { dept, subDept, jsonFormData, form_id } = req.body;
 
-  const form = await UserFormData.create({
+  const obj = await UserFormData.create({
     dept,
     subDept,
     jsonFormData,
     form_id,
   });
 
-  return res.status(201).json({ msg: "user data added" });
+  return res.status(201).json({ msg: "user data added", userId: obj._id });
 });
 
 const start = async () => {
